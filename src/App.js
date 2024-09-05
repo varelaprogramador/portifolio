e5e4a807ea6fe63e1868cc91982f6ce3f7db6700
Ryan varela <108085059+varelaprogramador@users.noreply.github.com>
2024-09-04 22:34:27 -0300

    update

--- src/App.js
+++ src/App.js
@@ -50,34 +50,34 @@ function App() {
           </figure>
         </div>
       </section>
-      <div data-aos="fade-bottom" className="division">
+      <div data-aos="fade-up" className="division">
         <img src={divisionImage}></img>
 
       </div>
       <section className="container-3">
 
-        <h2 data-aos="fade-bottom" className="titulo-padrao"><span className="enfase">Skills</span> Principais</h2>
-        <div className="grid-skills">
+        <h2 data-aos="fade-up" className="titulo-padrao"><span className="enfase">Skills</span> Principais</h2>
+        <div data-aos="fade-up" className="grid-skills">
 
-          <article className="card-skills">
+          <article  className="card-skills">
             <h2>Front end</h2>
             <p>Desenvolvimento de sites responsivos e interativos utilizando
               tecnologias modernas, como o React e Next,
               juntamente com bibliotecas populares como Tailwind CSS e Styled Components.</p>
           </article>
-          <article className="card-skills">
+          <article  className="card-skills">
             <h2>UI Desginer</h2>
             <p>Desenvolvimento de sites prototipos de alta fidelidade via figma.
               Sou altamente adptavel para criação de prototipos UI nao sou dependente de nenhuma ferramenta.</p>
 
           </article>
-          <article className="card-skills">
+          <article  className="card-skills">
             <h2>Automações</h2>
             <p>Comecei a pouco tempo a estudar automações. Sou capaz de criar automações complexas (Ainda estou estudando)</p>
 
           </article >
         </div>
-        <div className="legenda-skills">
+        <div  className="legenda-skills">
           <p >Todas essas skills e experiencias me proporcionaram uma linha de raciocinio bastante singular
             ou seja tenho bastante ideias para <span className="enfase">potencialização de negóçios.</span> </p>
         </div>
